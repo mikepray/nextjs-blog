261859606ebf9ad783cf1fefdf781705a2e3973a
mike.pray <mwpray@gmail.com>
2021-12-10 08:39:24 -0600

    moving a post to drafts, changing favicon and title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Mike Pray'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'blog.mikepray.dev'
 
 export default function Layout({ children, home }) {
   return (
